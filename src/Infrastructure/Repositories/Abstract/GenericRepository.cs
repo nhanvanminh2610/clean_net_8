8d0b0abeb9114d92aba28ef9bd6904716eb737d5
Nhan Van Minh <nhanvanminh2610@gmail.com>
2025-01-15 16:57:02 +0700

    feat: repository pattern

---
+++ src/Infrastructure/Repositories/Abstract/GenericRepository.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Repositories.Abstract;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Repositories.Abstract
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected ApplicationDbContext _dbContext;
+        protected DbSet<T> dbSet;
+        protected readonly ILogger _logger;
+
+        public GenericRepository(ApplicationDbContext dbContext = null)
+        {
+            _dbContext = dbContext;
+            dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task<bool> Add(T entity)
+        {
+            try
+            {
+                await dbSet.AddAsync(entity);
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error adding entity");
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<T>> All() => await dbSet.ToListAsync();
+
+        public async Task<bool> Delete(int id)
+        {
+            try
+            {
+                var entity = await dbSet.FindAsync(id);
+                if (entity != null)
+                {
+                    dbSet.Remove(entity);
+                    return true;
+                }
+                else
+                {
+                    _logger.LogWarning("Entity with id {Id} not found for deletion", id);
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error deleting entity with id {Id}", id);
+                return false;
+            }
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            try
+            {
+                return await dbSet.FindAsync(id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error getting entity with id {Id}", id);
+                return null;
+            }
+        }
+
+        public Task<bool> Upsert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
