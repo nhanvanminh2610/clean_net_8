df1741de0938375351b6c85d26ff3f912a2fa938
Nhan Van Minh <nhanvanminh2610@gmail.com>
2025-01-16 12:04:41 +0700

    feat: done repository

--- src/Domain/Repositories/Abstract/IGenericRepository.cs
+++ src/Domain/Repositories/Abstract/IGenericRepository.cs
@@ -1,14 +1,29 @@
-﻿namespace Domain.Repositories.Abstract
+﻿using System.Linq.Expressions;
+
+namespace Domain.Repositories.Abstract
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<IEnumerable<T>> All();
-        Task<T> GetById(int id);
+        IQueryable<T> AsQueryable();
 
-        Task<bool> Add(T entity);
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
 
-        Task<bool> Delete(int id);
+        Task<TResult> GetAsync<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, CancellationToken cancellationToken = default);
 
-        Task<bool> Upsert(T entity);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+
+        Task<List<TResult>> GetAllAsync<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, CancellationToken cancellationToken = default);
+
+        Task AddAsync(T entity, string id, CancellationToken cancellationToken = default);
+
+        Task AddAsync(IEnumerable<T> entities, string id, CancellationToken cancellationToken = default);
+
+        Task UpdateAsync(T entity, string id, CancellationToken cancellationToken = default);
+
+        Task UpdateAsync(IEnumerable<T> entities, string id, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(T entity, string id, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(IEnumerable<T> entities, string id, CancellationToken cancellationToken = default);
     }
 }
--- src/Infrastructure/Repositories/Abstract/GenericRepository.cs
+++ src/Infrastructure/Repositories/Abstract/GenericRepository.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
 
 namespace Infrastructure.Repositories.Abstract
 {
@@ -17,61 +18,96 @@ namespace Infrastructure.Repositories.Abstract
             dbSet = _dbContext.Set<T>();
         }
 
-        public async Task<bool> Add(T entity)
+        public virtual IQueryable<T> AsQueryable() => dbSet.AsNoTracking();
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
+            => AsQueryable().FirstOrDefaultAsync(predicate, cancellationToken);
+        
+        public Task<TResult> GetAsync<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, CancellationToken cancellationToken = default)
+            => AsQueryable().Where(predicate).Select(selector).FirstOrDefaultAsync(cancellationToken);
+
+        public Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default) 
+            => AsQueryable().Where(predicate).ToListAsync();
+
+        public Task<List<TResult>> GetAllAsync<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, CancellationToken cancellationToken = default) 
+            => AsQueryable().Where(predicate).Select(selector).ToListAsync();
+
+        public async Task AddAsync(T entity, string id, CancellationToken cancellationToken = default)
         {
-            try
+            UpdateFieldMethorAddEntityModel(entity, id);
+            await dbSet.AddAsync(entity);
+            return;
+        }
+        
+        public async Task AddAsync(IEnumerable<T> entities, string id, CancellationToken cancellationToken = default)
+        {
+            foreach (var entity in entities)
             {
-                await dbSet.AddAsync(entity);
-                return true;
+                UpdateFieldMethorAddEntityModel(entity, id);
             }
-            catch (Exception e)
+            await dbSet.AddRangeAsync(entities);
+            return;
+        }
+        
+        public Task UpdateAsync(T entity, string id, CancellationToken cancellationToken = default)
+        {
+            UpdateFieldMethorUpdateEntityModel(entity, id);
+            dbSet.Update(entity);
+            return Task.CompletedTask;
+        }
+        
+        public Task UpdateAsync(IEnumerable<T> entities, string id, CancellationToken cancellationToken = default)
+        {
+            foreach (var entity in entities)
             {
-                _logger.LogError(e, "Error adding entity");
-                return false;
+                UpdateFieldMethorUpdateEntityModel(entity, id);
             }
+            dbSet.UpdateRange(entities);
+            return Task.CompletedTask;
+        }
+        
+        public Task DeleteAsync(T entity, string id, CancellationToken cancellationToken = default)
+        {
+            dbSet.Remove(entity);
+            return Task.CompletedTask;
+        }
+        
+        public Task DeleteAsync(IEnumerable<T> entities, string id, CancellationToken cancellationToken = default)
+        {
+            dbSet.RemoveRange(entities);
+            return Task.CompletedTask;
         }
 
-        public async Task<IEnumerable<T>> All() => await dbSet.ToListAsync();
-
-        public async Task<bool> Delete(int id)
+        private void UpdateFieldMethorUpdateEntityModel(T entity, string userId)
         {
-            try
+            var properties = entity.GetType().GetProperties();
+            foreach (var property in properties)
             {
-                var entity = await dbSet.FindAsync(id);
-                if (entity != null)
+                if (property.Name == "LastUpdatedBy")
                 {
-                    dbSet.Remove(entity);
-                    return true;
+                    property.SetValue(entity, userId);
                 }
-                else
+                if (property.Name == "LastUpdatedTime")
                 {
-                    _logger.LogWarning("Entity with id {Id} not found for deletion", id);
-                    return false;
+                    property.SetValue(entity, DateTime.UtcNow);
                 }
             }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Error deleting entity with id {Id}", id);
-                return false;
-            }
         }
-
-        public async Task<T> GetById(int id)
+        private void UpdateFieldMethorAddEntityModel(T entity, string userId)
         {
-            try
+            var properties = entity.GetType().GetProperties();
+            foreach (var property in properties)
             {
-                return await dbSet.FindAsync(id);
+                if (property.Name == "CreatedBy")
+                {
+                    property.SetValue(entity, userId);
+                }
+                if (property.Name == "CreatedTime")
+                {
+                    property.SetValue(entity, DateTime.UtcNow);
+                }
             }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Error getting entity with id {Id}", id);
-                return null;
-            }
-        }
-
-        public Task<bool> Upsert(T entity)
-        {
-            throw new NotImplementedException();
+            UpdateFieldMethorUpdateEntityModel(entity, userId);
         }
     }
 }
