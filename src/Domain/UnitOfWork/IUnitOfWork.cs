e5337c534f2549be9a118978a62e47f1399a6657
Nhan Van Minh <nhanvanminh2610@gmail.com>
2025-01-16 14:58:13 +0700

    feat: implement UnitOfWork

---
+++ src/Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CompleteAsync();
+    }
+}
---
+++ src/Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Domain.UnitOfWork;
+using Infrastructure.Context;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public UnitOfWork()
+        {
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            using var transaction = _dbContext.Database.BeginTransaction();
+            try
+            {
+                var result = await _dbContext.SaveChangesAsync();
+                transaction.Commit();
+                return result;
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
